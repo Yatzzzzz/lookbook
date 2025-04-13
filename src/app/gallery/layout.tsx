1985dd422dd3c48e124440c4936a6d0a954863db
Yatzzzzz <itzikzabarsky@gmail.com>
2025-04-13 19:19:34 +0300

    Remove duplicate Lookbook heading from gallery layout

--- src/app/gallery/layout.tsx
+++ src/app/gallery/layout.tsx
@@ -7,21 +7,7 @@ export default function GalleryLayout({
 }) {
   return (
     <>
-      <header className="bg-white shadow-md">
-        <nav className="container mx-auto px-4 py-3">
-          <div className="flex items-center justify-between">
-            {/* Logo/Brand */}
-            <Link href="/" className="text-xl font-bold">
-              Lookbook
-            </Link>
-            
-            {/* Navigation Links - Removed as requested */}
-            <div className="flex space-x-6">
-              {/* Navigation links have been removed */}
-            </div>
-          </div>
-        </nav>
-      </header>
+      {/* Header section with duplicated "Lookbook" title removed */}
       <div>{children}</div>
       <footer>{/* Your footer content */}</footer>
     </>
